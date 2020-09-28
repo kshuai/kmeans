592754ac4e1a2e8d4034c5585e91cde3bfd07d48
kshuai <72010765+kshuai@users.noreply.github.com>
2020-09-28 17:19:09 +0800

    Add files via upload

---
+++ test_kmeans.py
@@ -0,0 +1,27 @@
+
+from numpy import *  
+import time  
+import matplotlib.pyplot as plt 
+import KMeans
+   
+## step 1: load data  
+print ("step 1: load data..." ) 
+dataSet = []   
+fileIn = open("D:/xuepython/testSet.txt")  
+for line in fileIn.readlines(): 
+	temp=[]
+	lineArr = line.strip().split('\t')  
+	temp.append(float(lineArr[0]))
+	temp.append(float(lineArr[1]))
+	dataSet.append(temp)
+
+fileIn.close()  
+## step 2: clustering...  
+print ("step 2: clustering..."  )
+dataSet = mat(dataSet) 
+k = 4  
+centroids, clusterAssment = KMeans.kmeans(dataSet, k)  
+  
+## step 3: show the result  
+print ("step 3: show the result..."  )
+KMeans.showCluster(dataSet, k, centroids, clusterAssment)
